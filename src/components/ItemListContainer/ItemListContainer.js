41a6f114705ff0288a00ea596bdad7e00dace803
Beto <tolerancia00@gmail.com>
2020-12-15 16:01:47 -0300

    clase 05: ItemCount

---
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+//function ItemListContainer(props) {
+    const ItemListContainer = ({titulo}) => {
+    return (
+      <>
+          <h1>{titulo}</h1>
+      </>
+    );
+  }
+  
+  export default ItemListContainer;
