5409924451ff13c34d595b5236f28ef9ef4b4bd8
Beto <tolerancia00@gmail.com>
2020-12-21 20:55:26 -0300

    Clase 06: Desafío: Catálogo con MAPS y Promises

---
+++ src/components/ItemList/ItemList.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect }  from 'react';
+import './ItemList.css';
+import { itemDetails } from './ItemArray';
+import Item from '../Item/Item';
+//function ItemListContainer(props) {
+    const ItemList = () => {
+        let [items, setItem] = useState([]);
+       
+    useEffect(() => {
+        setTimeout(() =>{
+            const promise = new Promise((resolve, reject) => {
+                resolve(itemDetails);
+            });
+            promise.then(itemDetails => {
+                setItem(itemDetails);
+            })
+        }, 3000)
+    },[])  
+    return (
+      <>
+            <div className="container">
+            {items === [] ? (
+            <p>cargando</p>
+            ) : (
+                <div>
+                {items.map((e, i) => {
+                    return (
+                        <Item 
+                        key={e.id}
+                        id={e.id}
+                        title={e.title}
+                        description={e.description}
+                        price={e.price}
+                        pictureURL={e.pictureURL}
+                        stock={e.stock}
+                    />
+                    )
+                    
+                })}
+                </div>
+            )}
+
+            
+            </div>
+      </>
+    );
+  }
+  
+  export default ItemList;
